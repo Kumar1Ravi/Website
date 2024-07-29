d1cd8febe028c31a7fa312191f06915dd025bf3e
Kumar Ravi <roshanrajan1073@gmail.com>
2024-07-29 16:25:51 +0530

    Updated_Final

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function getCurrentDate() {
 window.onload = function() {
     document.getElementById('current-date').textContent = getCurrentDate();
 }
+
+function downloadCV() {
+    const link = document.createElement('a');
+    link.href = 'D:\Website\My Resume.pdf';  // Replace with the actual URL of your CV
+    link.download = 'Your_CV.pdf';  // The default filename when the file is downloaded
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
